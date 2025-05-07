a4f0b120651f3ffe00eb23310f54fa3842d6488f
jec9386 <jerrychandev@gmail.com>
2025-05-07 05:44:08 -0400

    Room class created

---
+++ HotelOperations/src/main/java/com/pluralsight/Room.java
@@ -0,0 +1,31 @@
+package com.pluralsight;
+
+public class Room {
+
+    private int numberOfBeds;
+    private double price;
+    private boolean occupied;
+    private boolean dirty;
+    private boolean available;
+
+
+    public int getNumberOfBeds(){
+
+    }
+
+    public double getPrice(){
+
+    }
+
+    public boolean isOccupied(){
+
+    }
+
+    public boolean isDirty(){
+
+    }
+
+    public boolean isAvailable(){
+
+    }
+}
